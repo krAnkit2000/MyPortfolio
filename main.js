6ef7e00c064aa6e92dd133b2106110cb43721736
ankit <ankitkumar897955@gmail.com>
2023-10-11 13:33:36 +0530

    main.js upadate

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 
 
-
 function scrollToTop() {
     window.scrollTo(0, 0);
 }
 
+
+function connect_head() {
+    const element = document.getElementById("connect-head");
+    element.scrollIntoView();
+  }
