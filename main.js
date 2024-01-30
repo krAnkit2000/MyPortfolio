ca00b00f34d149faf806deb5e3bd2105775e72c0
Ankit Kumar <82326099+krAnkit2000@users.noreply.github.com>
2024-01-30 12:45:02 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 
-
 function scrollToTop() {
     window.scrollTo(0, 0);
 }
 
-
 function connect_head() {
-    const element = document.getElementById("connect-head");
+    const element = document.getElementById("contact");
     element.scrollIntoView();
   }
